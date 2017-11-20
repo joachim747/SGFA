a87e1b01261f697ea7a190a0849b77b3a808b0db
futzjug <achmed.lachnet@gmx.net>
2017-11-20 22:59:08 +0100

    Created a rising Bridge
    #386 time:3 state:InProgress comment:created the function to rise for a Bridge in the first Level, nicer Animation and modeling required

---
+++ SGFA_VPA_01/Assets/Bridge.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour {
+
+	Animator animator;
+	bool bridgeRisen;
+
+	// Use this for initialization
+	void Start () {
+		bridgeRisen = false;
+		animator = transform.parent.GetComponent<Animator>();
+		//animator = GetComponentInParent<Animator>();
+
+	}
+	
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag == "Player"){
+			bridgeRisen = true;
+			BridgeControl("Risen");
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if(col.gameObject.tag == "Player"){
+			bridgeRisen = false;
+			BridgeControl("Lowered");
+		}
+	}
+
+	void BridgeControl(string comand){
+		animator.SetTrigger(comand);
+	}
+
+}
