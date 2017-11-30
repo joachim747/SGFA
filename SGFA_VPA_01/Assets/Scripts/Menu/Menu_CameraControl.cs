a2ef4b80f2508e6c1f153ec436f45c07cb75fedb
joachim747 <32508829+joachim747@users.noreply.github.com>
2017-11-30 20:51:00 +0100

    Menu Update
    #424
    time:0.5
    comment: scene created for mainmenu and input-manager setup
    
    #427
    time:1
    comment:most of the logic for the moving camera is done. Loading of scenes and more will be implemented later
    
    #428
    time:0.5
    comment:general menudesign is clear now and will be implemented

---
+++ SGFA_VPA_01/Assets/Scripts/Menu/Menu_CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_CameraControl : MonoBehaviour {
+
+	public Transform m_CurrentMount;
+	public float m_Speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, m_CurrentMount.position, m_Speed);
+		transform.rotation = Quaternion.Slerp(transform.rotation, m_CurrentMount.rotation, m_Speed);
+	}
+
+	public void setMount(Transform newMount){
+		m_CurrentMount = newMount;
+	}
+}
