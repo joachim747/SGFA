569437dc2d4f34fff6fc7e64bdf7c2eebee70112
joachim747 <32508829+joachim747@users.noreply.github.com>
2017-11-30 21:59:48 +0100

    Menu Update V0.2
    #426
    time:0.5
    comment:first layout test with move-functionalities for settings and the possibility to quit the game

---
+++ SGFA_VPA_01/Assets/Scripts/Menu/Menu_Quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Quit : MonoBehaviour {
+
+	public void CloseApplication(){
+		Debug.Log("Game has been quitted");
+		Application.Quit();
+	}
+	
+}
