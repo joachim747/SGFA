08fe4af8fa709fc712d953ff4bd59c5eaf2d5008
joachim747 <32508829+joachim747@users.noreply.github.com>
2017-12-07 16:15:29 +0100

    Patch
    - Escaping the river is now possible
    - adjusted jump-ability of angel
    - players do now only take damage while in the river

---
+++ SGFA_VPA_01/Assets/Scripts/Zones/DeathZone_Stay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone_Stay : MonoBehaviour {
+
+	void OnTriggerStay(Collider col){
+		if(col.gameObject.tag == "Player"){
+			col.gameObject.GetComponent<PlayerHealth>().TakeDamage(.5f);
+		}
+	}
+}
