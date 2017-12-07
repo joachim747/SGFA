f84fcf52b2abed696b374a708956dd3ff1821158
joachim747 <32508829+joachim747@users.noreply.github.com>
2017-12-07 16:05:48 +0100

    Jump-Ability for Angel implemented
    #323
    time:0.5
    state:Done
    comment:applied script to character, made a second script to make jump more realistic, needs to be tested for further implementation
    
    #322
    time:1
    state:Done
    comment:implemented two scripts, one for jumping in general and one for more realistic jumping using advanced physics settings, might be replaced by a hovering effect, needs to be tested

---
+++ SGFA_VPA_01/Assets/Scripts/Characters/Ability_Jump.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability_Jump : MonoBehaviour {
+
+	[Range(1,10)] public float m_JumpVelocity;
+
+	void Update(){
+
+		if(Input.GetButtonDown("Jump")){
+			GetComponent<Rigidbody>().velocity = Vector3.up * m_JumpVelocity;
+		}
+		
+	}
+}
---
+++ SGFA_VPA_01/Assets/Scripts/Characters/Modificator_Jump.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Modificator_Jump : MonoBehaviour {
+
+	public float m_FallMultiplier=2.5f;
+	public float m_LowJumpMultiplier=2f;
+
+	Rigidbody player;
+
+	void Awake(){
+		player = GetComponent<Rigidbody>();
+	}
+
+	void Update(){
+		if(player.velocity.y<0){
+			player.velocity += Vector3.up * Physics.gravity.y * (m_FallMultiplier - 1) * Time.deltaTime;
+		}
+	}
+}
