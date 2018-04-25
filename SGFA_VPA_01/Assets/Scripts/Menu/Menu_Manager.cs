43428b37c08b622ac387afd08b02d18b8b1c1337
joachim747 <32508829+joachim747@users.noreply.github.com>
2018-04-25 14:47:58 +0200

    Hotfix
    quitting the application once build is again possible

--- SGFA_VPA_01/Assets/Scripts/Menu/Menu_Manager.cs
+++ SGFA_VPA_01/Assets/Scripts/Menu/Menu_Manager.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class Menu_Manager : MonoBehaviour {
 
 	public void CloseApplication(){
-		Debug.Log("Game has been quitted");
+		Application.Quit();
 	}
 
 	public void OpenWebsite(string url){
