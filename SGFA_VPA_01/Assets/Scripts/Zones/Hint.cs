48cd2b43c53143b029b05d04c74558bb5c3adb89
joachim747 <32508829+joachim747@users.noreply.github.com>
2018-04-27 19:19:37 +0200

    Third Level is almost finished
    New Feature has been implemented.
    Logic is finalized.
    Some GameObjects have to be replaced and colored, this will be done later and timetracked.

---
+++ SGFA_VPA_01/Assets/Scripts/Zones/Hint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hint : MonoBehaviour {
+	
+	public GameObject image;
+	private int playerInZone = 0;
+
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag == "Player"){
+			playerInZone++;
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if(col.gameObject.tag == "Player"){
+			playerInZone--;
+		}
+	}
+
+	void Update () {
+		if(playerInZone>0){
+			image.SetActive(true);
+		} else {
+			image.SetActive(false);
+		}
+	}
+}
