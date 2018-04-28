13e06f09c888e0c10aa12c0240c3b8063bb32d36
joachim747 <32508829+joachim747@users.noreply.github.com>
2018-04-28 14:56:54 +0200

    Reworked Devil-Ability
    #544
    time: 2
    state: Done
    comment: Devil now only destroyes boxes when in range and player hits right handside shift on the keyboard.
    
    #543
    state: Done

---
+++ SGFA_VPA_01/Assets/Scripts/Characters/Ability_Destroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability_Destroy : MonoBehaviour {
+
+	private Collider collider;
+
+	void OnTriggerEnter(Collider col){
+		collider = col;
+	}
+
+	void OnTriggerExit(){
+		collider = null;
+	}
+
+	void Update(){
+		if(Input.GetButtonDown("Destroy")){
+			if(collider != null && collider.gameObject.tag == "Box"){
+				Destroy(collider.gameObject);
+			}
+		}
+	}
+}
