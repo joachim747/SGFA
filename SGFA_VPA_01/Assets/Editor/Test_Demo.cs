23753543bf9d368d48d1b2dbe04a3dab738e0a25
joachim747 <32508829+joachim747@users.noreply.github.com>
2018-05-04 09:18:09 +0200

    Added Demo Test
    Basic Functionality for testing is now implemented

---
+++ SGFA_VPA_01/Assets/Editor/Test_Demo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.TestTools.Utils;
+
+public class Test_Demo {
+
+	[Test]
+	public void GameObject_CreatedWithGiven_WillHaveTheName()
+	{
+		var test = "DemoObject";
+		var go = new GameObject(test);
+		Assert.AreEqual(test, go.name);
+	}
+}
