bd8edc41c4bce1b8fc553e88bc01d863c5281577
joachim747 <32508829+joachim747@users.noreply.github.com>
2018-05-29 13:25:55 +0200

    Sprint_11: Update AI
    #331
    time:0.5
    state: Done
    comment: it is now possible to take health of ai
    
    #333
    time:0.5
    state: Done
    comment: AI recognizes if it is defeated
    
    #334
    time:0.5
    state: Done
    comment: ai gets destroyed if so. possibility to include animations is given.

---
+++ SGFA_VPA_01/Assets/Scripts/AI/AIHealth.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public float m_StartingHealth = 50f;
+	public Slider m_Slider;
+	public Image m_FillImage;
+	public Color m_FullHealthColor = Color.green;
+	public Color m_ZeroHealthColor = Color.red;
+	//public GameObject m_ExplosionPrefab;
+
+	private float m_CurrentHealth;
+
+	private void OnEnable(){
+		m_CurrentHealth = m_StartingHealth;
+
+		SetHealthUI();
+	}
+
+	public void TakeDamage(float amount){
+		m_CurrentHealth -= amount;
+
+		SetHealthUI();
+
+		if(m_CurrentHealth <= 0f){
+			OnDeath();
+		}
+	}
+
+	private void SetHealthUI(){
+		m_Slider.value = m_CurrentHealth;
+
+		m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
+	}
+
+	private void OnDeath(){
+		//effects & sounds? menues? 
+		gameObject.Destroy();
+	}
+
+	public bool getIfDead(){
+		return m_Dead;
+	}
+}
