6d3c550b012039f40cb381d8ccf4a6142937b717
joachim747 <32508829+joachim747@users.noreply.github.com>
2018-06-01 11:08:13 +0200

    Adjustment to Lvl 4
    - enhanced navmesh links
    - added enemies
    - added Levelcondition, so that player has to defeat all enemies to activate the triggerzone
    - adjusted the message of NPC
    - changed Starting information
    - added information when riddle has been solved and how many enemies are left(not shown yet)

---
+++ SGFA_VPA_01/Assets/Scripts/Manager/LevelGoal_Enemy.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelGoal_Enemy : MonoBehaviour {
+
+	public GameObject m_Zone;
+	public List<GameObject> enemies = new List<GameObject>();
+	public GameObject infobox;
+	public Text message;
+
+	private WaitForSeconds m_InfoWait = new WaitForSeconds(3f);
+
+	// Use this for initialization
+	void Start () {
+		m_Zone.SetActive(false);
+	}
+
+	void Update(){
+		foreach (GameObject go in enemies) {
+			if(!go.active){
+				enemies.Remove(go);
+			}
+		}
+
+		switch(enemies.Count){
+			case 0:
+				handleMessage("All Enemies defeated. Zone is now reachable.");
+				m_Zone.SetActive(true);
+				break;
+			case 1:
+				handleMessage("One Enemy left");
+				break;
+			case 2:
+				handleMessage("Two Enemies left");
+				break;
+		}
+	}
+
+	private IEnumerator handleMessage(string msg){
+		message.text = msg;
+		infobox.SetActive(true);
+		yield return m_InfoWait;
+		infobox.SetActive(false);
+	}
+
+	/* below code would be nicer but causes serious issues
+	void Start () {
+		m_Zone.SetActive(false);
+		StartCoroutine(checkForCondition());
+	}
+	
+	private IEnumerator checkForCondition(){
+		while(!m_Zone.active){
+			foreach (GameObject go in enemies) {
+				if(!go.active){
+					enemies.Remove(go);
+				}
+			}
+
+			if(enemies.Count == 0){
+				m_Zone.SetActive(true);
+			}
+		}
+
+		yield return null;
+	}
+ */
+}
