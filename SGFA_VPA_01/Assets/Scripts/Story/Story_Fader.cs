3349087a7150632203b346db655c7fc468a3bae6
joachim747 <32508829+joachim747@users.noreply.github.com>
2018-06-10 18:55:43 +0200

    Added basics for future fading
    ... in story mode

---
+++ SGFA_VPA_01/Assets/Scripts/Story/Story_Fader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Story_Fader : MonoBehaviour {
+
+	public Image m_UI;
+
+	public void StartFade(){
+		m_UI.SetActive(true);
+	}
+}
