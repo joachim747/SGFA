b233c70e294acf7972f3f3f8ce9be45f7520d65c
joachim747 <32508829+joachim747@users.noreply.github.com>
2018-06-11 10:28:20 +0200

    Added VideoManager
    Now shows a video in the beginning of the game, which is also skippable.

---
+++ SGFA_VPA_01/Assets/Scripts/Manager/VideoManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoManager : MonoBehaviour {
+
+	public float m_Seconds;
+	public string m_Scene;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(WaitandLoad(m_Seconds, m_Scene));
+	}
+
+	void Update(){
+		if(Input.GetMouseButtonDown(0) || Input.GetKeyDown("space")){
+			SceneManager.LoadScene(m_Scene);
+		}
+	}
+
+	private IEnumerator WaitandLoad(float seconds, string scene){
+		yield return new WaitForSeconds(seconds);
+		SceneManager.LoadScene(scene);
+	}
+}
