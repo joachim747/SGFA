c7ef36a74ad7a5faa5fe2d9660ad87ce276b654a
futzjug <achmed.lachnet@gmx.net>
2018-06-12 14:21:29 +0200

    Jump animation works

--- SGFA_VPA_01/Assets/Scripts/Characters/Ability_Jump.cs
+++ SGFA_VPA_01/Assets/Scripts/Characters/Ability_Jump.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class Ability_Jump : MonoBehaviour {
 
 	[Range(1,10)] public float m_JumpVelocity = 4;
+    public ParticleSystem jumpParticles;
+    private Animator anim;
+    
 
-	void Update(){
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update(){
 
 		if(Input.GetButtonDown("Jump")){
-			GetComponent<Rigidbody>().velocity = Vector3.up * m_JumpVelocity;
+            anim.SetTrigger("Jump");
+            StartCoroutine(Jump());
+            
 		}
 		
 	}
+
+    IEnumerator Jump()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Instantiate(jumpParticles, transform.position, Quaternion.identity);
+        GetComponent<Rigidbody>().velocity = Vector3.up * m_JumpVelocity;
+    }
 }
