96d77ba703f89dfdcdbb0bcc95630d738642118a
futzjug <achmed.lachnet@gmx.net>
2018-06-13 21:40:20 +0200

    Devil Animations integrated

--- SGFA_VPA_01/Assets/Scripts/Characters/Ability_Destroy.cs
+++ SGFA_VPA_01/Assets/Scripts/Characters/Ability_Destroy.cs
@@ -4,31 +4,45 @@ using UnityEngine;
 
 public class Ability_Destroy : MonoBehaviour {
 
-	private List<Collider> collider = new List<Collider>();
+    public GameObject hit;
+    private Animator anim;
+    private Hitbox hitbox;
 
-	void OnTriggerEnter(Collider col){
-		collider.Add(col);
-	}
-
-	void OnTriggerExit(Collider col){
-		collider.Remove(col);
-	}
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        hitbox = hit.GetComponent<Hitbox>();
+    }
 
 	void Update(){
-		if(Input.GetButtonDown("Attack") && collider.Count > 0){
-			foreach (Collider go in collider) {
-				if(go.gameObject.tag=="Box"){
-					go.GetComponent<Destroyable>().destroyObject();
-					collider.Remove(go);
-				}
-				if(go.gameObject.tag=="Enemy"){
-					go.GetComponent<AIHealth>().TakeDamage(15f);
+		if(Input.GetButtonDown("Attack") && hitbox.getItemsInHitbox().Count > 0){
 
-					if(go.GetComponent<AIHealth>().GetHealth() <= 0f){
-						collider.Remove(go);
-					}
-				}
-			}
-		}
+            anim.SetTrigger("Attack");
+            StartCoroutine(Attack());
+
+        }
 	}
+
+    IEnumerator Attack()
+    {
+       
+        yield return new WaitForSeconds(0.3f);
+        foreach (Collider go in hitbox.collider)
+        {
+            if (go.gameObject.tag == "Box")
+            {
+                go.GetComponent<Destroyable>().destroyObject();
+                hitbox.collider.Remove(go);
+            }
+            if (go.gameObject.tag == "Enemy")
+            {
+                go.GetComponent<AIHealth>().TakeDamage(15f);
+
+                if (go.GetComponent<AIHealth>().GetHealth() <= 0f)
+                {
+                    hitbox.collider.Remove(go);
+                }
+            }
+        }
+    }
 }
---
+++ SGFA_VPA_01/Assets/Scripts/Characters/Hitbox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour {
+
+    public List<Collider> collider = new List<Collider>();
+
+    void OnTriggerEnter(Collider col)
+    {
+        collider.Add(col);
+        //Debug.Log(col.name + " entered");
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        collider.Remove(col);
+    }
+
+    public List<Collider> getItemsInHitbox()
+    {
+        return collider;
+    }
+
+}
